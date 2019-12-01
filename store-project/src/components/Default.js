ce07fad0a50ad5fb3d41e8e2108a864b91412869
Saad Hmed <saad.ahmed2696@gmail.com>
2019-12-02 00:38:19 +0500

    components added

---
+++ store-project/src/components/Default.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Default extends Component {
+    render() {
+        return (
+            <div>
+                <h3>We are not here yet!</h3>
+            </div>
+        )
+    }
+}
